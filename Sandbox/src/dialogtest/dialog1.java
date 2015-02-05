69115d9431fbba73027cd90717d8db710a749b6e
GriffTech <GriffTechs@gmail.com>
2015-02-05 19:45:13 +0000

    Initial Commit

---
+++ Sandbox/src/dialogtest/dialog1.java
@@ -0,0 +1,42 @@
+package dialogtest;
+
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+
+public class dialog1 extends JFrame{
+	private JTextField item1;
+	private JTextField item2;
+	private JTextField item3;
+	private JPasswordField passwordField;;
+	
+	public dialog1() {
+		super("The title");
+		setLayout(new FlowLayout());
+		
+		item1 = new JTextField(10);
+		add(item1);
+		
+		item2 = new JTextField(10);
+		add(item2);
+		
+		item3 = new JTextField(10);
+		add(item3);
+		
+		passwordField = new JPasswordField(10);
+		add(passwordField);	
+		
+
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Sandbox/src/dialogtest/showdialog1.java
@@ -0,0 +1,15 @@
+package dialogtest;
+
+import javax.swing.JFrame;
+
+public class showdialog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dialog1 thisdialog = new dialog1();
+		thisdialog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		thisdialog.setSize(200,200);
+		thisdialog.setVisible(true);
+	}
+
+}
