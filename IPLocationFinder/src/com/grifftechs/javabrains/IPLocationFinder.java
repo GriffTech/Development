d63d19a89d0ff5e8162e4f8d459349860516f67f
GriffTech <GriffTechs@gmail.com>
2015-02-24 17:12:37 +0000

    2015-02-24 Proxy addition possible #2

--- IPLocationFinder/src/com/grifftechs/javabrains/IPLocationFinder.java
+++ IPLocationFinder/src/com/grifftechs/javabrains/IPLocationFinder.java
@@ -28,11 +28,19 @@ public class IPLocationFinder {
 			options.setProperty(HTTPConstants.PROXY, proxyProperties);
 			client.setOptions(options);
 			*/
-			
-			
-			
+						
 			String ipaddress = args[0];
 			GeoIPService ipservice = new GeoIPService();
+			
+			/*
+			System.setProperty("http.proxyHost", "192.155.1168.101");
+			System.setProperty("http.proxyPort", "80");
+			System.setProperty("https.proxyHost", "192.167.134.103");
+			System.setProperty("https.proxyPort", "81");
+			System.setProperty("http.nonProxyHosts","192.168.170.105|192.168.170.104");
+			*/
+			
+			
 			//ipservice.getGeoIPServiceSoap();
 			GeoIPServiceSoap geoIPServiceSoap = ipservice.getGeoIPServiceSoap();
 			GeoIP geoip = geoIPServiceSoap.getGeoIP(ipaddress);
