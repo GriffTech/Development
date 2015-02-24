1a5339c833a0a716ba701e7c3422d75291861ed3
GriffTech <GriffTechs@gmail.com>
2015-02-24 17:05:12 +0000

    2015-02-24 - Added possibly proxy update

--- IPLocationFinder/src/com/grifftechs/javabrains/IPLocationFinder.java
+++ IPLocationFinder/src/com/grifftechs/javabrains/IPLocationFinder.java
@@ -11,6 +11,26 @@ public class IPLocationFinder {
 		if (args.length != 1){
 			System.out.println("You need to supply an argument");
 		} else {
+			
+			
+			/*
+			ServiceClient client = new ServiceClient(); // build your service client here
+
+			Options options = new Options();
+
+			HttpTransportProperties.ProxyProperties proxyProperties = new HttpTransportProperties.ProxyProperties();
+			proxyProperties.setDomain("");
+			proxyProperties.setProxyName("");
+			proxyProperties.setProxyPort(8000);
+			proxyProperties.setUserName("");
+			proxyProperties.setPassWord("");
+
+			options.setProperty(HTTPConstants.PROXY, proxyProperties);
+			client.setOptions(options);
+			*/
+			
+			
+			
 			String ipaddress = args[0];
 			GeoIPService ipservice = new GeoIPService();
 			//ipservice.getGeoIPServiceSoap();
