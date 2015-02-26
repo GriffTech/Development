b1c1e2c76703e6d66113024e751311093e53f060
GriffTech <grifftechs@gmsil.com>
2015-02-26 11:21:59 +0000

    2015-02-26 Tosh New module - beginnersGuide

---
+++ Sandbox/src/com/grifftech/helloworld/beginnersGuide.java
@@ -0,0 +1,19 @@
+package com.grifftech.helloworld;
+
+public class beginnersGuide {
+
+	public static void main(String[] args) {
+		// Basic Println
+		System.out.println("Test1");
+		// For Loops
+		int sum, i;
+		sum=0;
+		for (i = 0; i < 10; i++) {
+			sum=sum+i;
+		}
+		System.out.println("Sum:"+sum);
+		
+
+	}
+
+}
